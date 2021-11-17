517a68d9d8f9fd76130be9d333b7075f0860a1f0
Abby <yugram7@gmail.com>
2021-11-16 19:48:00 -0500

    Ejercicios Markdown

---
+++ R.R
@@ -0,0 +1,12 @@
+git init # Iniciar un repositorio
+git status # Saber el estado del repositorio
+git add # Agregar archivos al repositorio
+git rm # Quitar archivos del repositorio
+git commit -m "Mensaje"
+clear # Limpiar git bash
+git log 
+git checkout 
+git checkout master
+
+
+
